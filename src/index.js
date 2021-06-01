d7b8e3cf4e0f2b899997ee39b1978ec2b967a561
FlameFury20 <horjarazvan@gmail.com>
2021-06-01 10:54:03 +0300

    Restructered the file hierarchy

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, Menu, Notification, nativeTheme, globalShortcut } = require('electron');
 const path = require('path');
+const os = require('os');
+
+app.setAppUserModelId('Google Classroom');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
 
+// Check for internet connection;
+
 let checkConnectionWindow;
 var ConnStatus;
 
 app.whenReady().then(() => {
   checkConnectionWindow = new BrowserWindow({ width: 0, height: 0, show: false, webPreferences: { nodeIntegration: true } });
-  checkConnectionWindow.loadFile(path.join(__dirname, 'checkConnection.html'));
+  checkConnectionWindow.loadFile(path.join(__dirname, 'html/checkConnection.html'));
 });
 
 ipcMain.on('update-online-connection', (event, status) => {
@@ -19,37 +24,104 @@ ipcMain.on('update-online-connection', (event, status) => {
   console.log(ConnStatus);
 });
 
+// Menu Bar
+
+const isMac = process.platform === 'darwin';
+const MenuTemplate = [
+  ...(isMac ? [{
+    label: app.name,
+  }] : []),
+  {
+    label: app.name,
+    submenu: [
+      { role: 'minimize' },
+      { role: 'togglefullscreen' },
+      isMac ? { role: 'close' } : { role: 'quit' }
+    ]
+  },
+  {
+    label: "Edit",
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac ? [
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' },
+        { type: 'separator' }
+      ] : [
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectAll' }
+      ])
+    ]
+  },
+  {
+    label: "Help",
+    submenu: [
+      {
+        label: "Info",
+        click: async () => {
+          const { shell } = require('electron');
+          await shell.openExternal('https://google.com/');
+        }
+      },
+    ]
+  }
+];
+
+// Create the main window
+
 const createWindow = () => {
   // Create the browser window.
+  nativeTheme.themeSource = 'light';
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    autoHideMenuBar: true,
+    backgroundColor: "#fff",
     webPreferences: {
       nodeIntegration: true,
-      enableRemoteModule: true
+      enableRemoteModule: true,
+      contextIsolation: true
     },
     icon: path.join(__dirname, 'images/classroom.ico')
   });
 
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+  mainWindow.setThumbarButtons([
+    {
+      tooltip: "Button 1",
+      icon: "./src/images/classroom.ico",
+      click() { showNotification("Google Classroom", "Thumbnail Button Pressed") }
+    }
+  ]);
+
+  // Load in the menu bar
+
+  const menu = Menu.buildFromTemplate(MenuTemplate);
+  Menu.setApplicationMenu(menu);
+
   if (ConnStatus === 'online') {
     // and load the index.html of the app.
-    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+    mainWindow.loadFile(path.join(__dirname, 'html/index.html'));
     setTimeout(() => {
-      mainWindow.loadFile(path.join(__dirname, 'warning.html'));
+      mainWindow.loadFile(path.join(__dirname, 'html/warning.html'));
     }, 3000);
     setTimeout(() => {
       mainWindow.loadURL('https://classroom.google.com/signin/');
     }, 8000);
   } else {
-    mainWindow.loadFile(path.join(__dirname, 'offline.html'));
-    // setTimeout(() => { app.relaunch() }, 30000);
+    mainWindow.loadFile(path.join(__dirname, 'html/offline.html')).then(() => { showNotification("Offline", "You are offline. Check your internet connection!"); });
+    setTimeout(() => { app.relaunch() }, 30000);
   }
   checkConnectionWindow.destroy();
 }
-
-// Open the DevTools.
-// mainWindow.webContents.openDevTools();
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -59,7 +131,7 @@ app.on('ready', () => {
   setTimeout(() => {
     createWindow();
   }, 1000);
-})
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -80,3 +152,12 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+function showNotification(title, body) {
+  const notification = {
+    icon: "./src/images/classroom.ico",
+    title: title,
+    body: body
+  }
+  new Notification(notification).show();
+}
