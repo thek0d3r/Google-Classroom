3de9c882d7728d0a2a8444dbe532de1f5596e3f7
FlameFury20 <horjarazvan@gmail.com>
2021-06-04 16:22:09 +0300

    Updated the app icon logic

--- src/index.js
+++ src/index.js
@@ -87,7 +87,7 @@ const createWindow = () => {
       enableRemoteModule: true,
       contextIsolation: true
     },
-    icon: nativeImage.createFromPath(path.join(__dirname, "images/classroom.png"))
+    icon: (process.platform === "linux") ? path.join(__dirname, "images/classroom.png") : (process.platform === "win32") ? nativeImage.createFromPath(path.join(__dirname, "images/classroom.ico")) : nativeImage.createFromPath(path.join(__dirname, "images/classroom.png"))
   });
 
   mainWindow.once("ready-to-show", () => {
